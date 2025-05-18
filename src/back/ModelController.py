219b95a942935e85ba0d6894b533548ff8d888bb
hdelacruzmo <hdelacruzmo@gmail.com>
2025-05-18 09:47:42 -0500

    Update ModelController.py

--- src/back/ModelController.py
+++ src/back/ModelController.py
@@ -12,10 +12,10 @@ class ModelController:
         print("ModelController.__init__ ->")
 
         self.model_path = osp.join(Definitions.ROOT_DIR, "resources/models")
-        self.rf_model_path = osp.join(self.model_path, "rf_model.joblib")
+        # self.rf_model_path = osp.join(self.model_path, "rf_model.joblib")
         self.maxent_model_path = osp.join(self.model_path, "maxent_model.joblib")
 
-        self.rf_model = joblib.load(self.rf_model_path)
+        # self.rf_model = joblib.load(self.rf_model_path)
         self.maxent_model = joblib.load(self.maxent_model_path)
 
         self.input_df = ""
@@ -36,18 +36,18 @@ class ModelController:
     def predict(self):
         X_test, Y_test = self.d_processing.transform(self.input_df)
 
-        # RF
-        y_pred_rf = self.rf_model.predict(X_test)
-        y_pred_proba_rf = self.rf_model.predict_proba(X_test)
-        y_pred_proba_rf = np.round(y_pred_proba_rf * 100, 2)
+        # --- RF DESACTIVADO ---
+        # y_pred_rf = self.rf_model.predict(X_test)
+        # y_pred_proba_rf = self.rf_model.predict_proba(X_test)
+        # y_pred_proba_rf = np.round(y_pred_proba_rf * 100, 2)
 
-        rf_df = X_test.copy()
-        rf_df["Real"] = Y_test.values
-        rf_df["Predicción"] = y_pred_rf
-        rf_df[f"Prob. Clase 0 RF (%)"] = y_pred_proba_rf[:, 0]
-        rf_df[f"Prob. Clase 1 RF (%)"] = y_pred_proba_rf[:, 1]
+        # rf_df = X_test.copy()
+        # rf_df["Real"] = Y_test.values
+        # rf_df["Predicción"] = y_pred_rf
+        # rf_df[f"Prob. Clase 0 RF (%)"] = y_pred_proba_rf[:, 0]
+        # rf_df[f"Prob. Clase 1 RF (%)"] = y_pred_proba_rf[:, 1]
 
-        # MaxEnt
+        # --- MaxEnt ---
         y_pred_maxent = self.maxent_model.predict(X_test)
         y_pred_proba_maxent = self.maxent_model.predict_proba(X_test)
         y_pred_proba_maxent = np.round(y_pred_proba_maxent * 100, 2)
@@ -58,13 +58,11 @@ class ModelController:
         maxent_df[f"Prob. Clase 0 MaxEnt (%)"] = y_pred_proba_maxent[:, 0]
         maxent_df[f"Prob. Clase 1 MaxEnt (%)"] = y_pred_proba_maxent[:, 1]
 
-        # Full view
+        # Full view (solo MaxEnt)
         full_df = X_test.copy()
         full_df["Real"] = Y_test.values
-        full_df["Pred. RF"] = y_pred_rf
         full_df["Pred. MaxEnt"] = y_pred_maxent
-        full_df["Prob. RF (1)"] = y_pred_proba_rf[:, 1]
         full_df["Prob. MaxEnt (1)"] = y_pred_proba_maxent[:, 1]
 
-        return rf_df, maxent_df, full_df
+        return maxent_df, full_df
 
